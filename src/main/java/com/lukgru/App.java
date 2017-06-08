b809c486e076502d1cef9b22244d9e42abdeb487
LukaszGruba <lukgru@gmail.com>
2017-06-08 20:06:44 +0200

    init

---
+++ src/main/java/com/lukgru/App.java
@@ -0,0 +1,13 @@
+package com.lukgru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
