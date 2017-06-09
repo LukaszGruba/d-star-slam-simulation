6b113d52a725a30e33d1dbebc7bfa30ab26c5259
LukaszGruba <lukgru@gmail.com>
2017-06-09 22:56:49 +0200

    added TODOs + basic example

--- src/main/java/com/lukgru/slam/Main.java
+++ src/main/java/com/lukgru/slam/Main.java
@@ -2,33 +2,47 @@ package com.lukgru.slam;
 
 import com.lukgru.slam.dstar.DStarLite;
 
-import java.util.Random;
-
 /**
  * Created by Lukasz on 08.06.2017.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Random r = new Random();
+    /**
+     * TODO: general algorithm:
+     * 1. set position as 0,0
+     * 2. detect visible obstacles
+     * 3. update map
+     * 4. replan route
+     * 5. move one step
+     * 6. if at goal -> end, else goto 2
+     */
 
-        //Create pathfinder
+    //TODO: at the beginning try to find goal, if not present then make some assumption that it can be behind obstacle so go to the nearest edge of obstacle and then
+
+    //TODO: remember all of the found checkpoint positions
+
+    public static void main(String[] args) {
         DStarLite pf = new DStarLite();
-        //set start and goal nodes
-        pf.init(0, 0, 1000, 1000);
-        //set impassable nodes
-        for (int i = 0; i < 20000; i++) {
-            pf.updateCell(r.nextInt(1000), r.nextInt(1000), -1);
+        pf.init(0, 0, 0, 10);
+        for (int i = -3; i < 10; i++) {
+            pf.updateCell(i, 3, -1);
         }
 
-        System.out.println("map initialised");
-
-        //perform the pathfinding
         pf.replan();
+        printPath(pf);
 
-        System.out.println("path found");
+        System.out.println("moved to -4, 3");
+        pf.updateStart(-4, 3);
+        for (int i = 0; i < 20; i++) {
+            pf.updateCell(-3, 3 + i, -1);
+        }
+        System.out.println("adding obstacles");
+        pf.replan();
+        System.out.println("replanning");
+        printPath(pf);
+    }
 
-        //get and print the path
+    private static void printPath(DStarLite pf) {
         pf.getPath()
                 .forEach(i -> System.out.println("x: " + i.x + " y: " + i.y));
     }
